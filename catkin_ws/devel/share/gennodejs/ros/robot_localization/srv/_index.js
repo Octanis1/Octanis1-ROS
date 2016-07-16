837d84348b912e2b0cc6c050e3e2ce11674b7acd
Sam Sulaimanov <sam@bitmorse.com>
2016-07-16 21:42:54 +0000

    ncovariance exploding. verify if imu info is good

---
+++ catkin_ws/devel/share/gennodejs/ros/robot_localization/srv/_index.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let SetDatum = require('./SetDatum.js')
+let SetPose = require('./SetPose.js')
+
+module.exports = {
+  SetDatum: SetDatum,
+  SetPose: SetPose,
+};
---
+++ catkin_ws/devel/share/robot_localization/cmake/robot_localization-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(robot_localization_MESSAGE_FILES "")
+set(robot_localization_SERVICE_FILES "/home/rover/Octanis1-ROS/catkin_ws/src/robot_localization/srv/SetPose.srv;/home/rover/Octanis1-ROS/catkin_ws/src/robot_localization/srv/SetDatum.srv")
