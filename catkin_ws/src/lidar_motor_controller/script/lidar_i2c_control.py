03e356015c1209f4a01a9aa16216f41447855a9b
Sam Sulaimanov <sam@bitmorse.com>
2016-07-25 15:40:41 +0000

    Lidar PI motor : working

--- catkin_ws/src/lidar_motor_controller/script/lidar_i2c_control.py
+++ catkin_ws/src/lidar_motor_controller/script/lidar_i2c_control.py
@@ -2,7 +2,7 @@
 
 import os
 import rospy
-#from time import sleep
+import time
 
 from std_msgs.msg import Float64, UInt16
 from pyA20 import i2c
@@ -10,10 +10,10 @@ from pyA20 import i2c
 from mavros.utils import *
 
 #Initialize module to use /dev/i2c-2
-i2c.init("/dev/i2c-2") 
+i2c.init("/dev/i2c-2")
 
 def set_lidar_motor(speed, direction):
-    i2c.open(0x0f) #The slave device address is 0x55
+    i2c.open(0x0f) #The slave device address is 0x0f
     #set speed and direction
     i2c.write([0x82, speed, speed]) #speed register, speed a, speed b
 
@@ -25,23 +25,30 @@ def set_lidar_motor(speed, direction):
     i2c.write([0xaa, direction, 0x01]) #direction register, direction, pa$
     i2c.close() #End communication with slave device
 
+
 def ramp_up():
-   i=0
+   i=10
    while(i<100):
        i += 10
        set_lidar_motor(i,0b1010)
        time.sleep(0.1)
 
+
 def motor_input_callback(v):
     v_int = v.data
     set_lidar_motor(v_int, 0b1010)
+
 
 def i2c_listener():
    rospy.init_node('i2c_listener', anonymous=True)
    rospy.Subscriber('motor_input', UInt16, motor_input_callback, queue_size=1)
 
+
 if __name__ == '__main__':
+
    print("ramping up motor")
    ramp_up() #starts motor
+
    i2c_listener()
+
    rospy.spin()
--- catkin_ws/src/lidar_motor_controller/script/rpms2volts.py
+++ catkin_ws/src/lidar_motor_controller/script/rpms2volts.py
@@ -7,32 +7,33 @@ from std_msgs.msg import Float64
 
 
 def convert_to_volts_callback(r):
-    print(r.data)
     new_rpms = float(current_rpms) + r.data
-    
+
     # We receive the information of the control_effort topic that indicates the value in rpms. Convert it into volts
     # We have in normal conditions : 3V -> 190 rpms
     # Volts value should be in pwm duty cycle
-    pwm_dc0 = round(0.35*255) # 3V*255/11V
-    rpm0 = 190
-    
-    pwm_dc = float(new_rpms)*(pwm_dc0/rpm0)
+    #pwm_dc0 = round(0.35*255) # 3V*255/11V
+    #pwm0 = 80 # 190 rpms in normal conditions
+    #rpm0 = 190
+    #pwm = round( float(new_rpms)*(pwm_dc0/rpm0) )
+
+
+    # According to calibration of lidar in normal conditions : VAL_PWM = 0,16*RPMS + 48
+
+    pwm = round( 0.16*new_rpms + 48 )
 
     # Check that we don't exceed the maximum and minimum voltage
-    # >>>>>>>>> Not sure of min value...
-    pwm_dc_max = 255
-    pwm_dc_min = round(0.1*255) # ? rpm
-    if pwm_dc > pwm_dc_max:
-        pwm_dc = pwm_dc_max
-    elif pwm_dc < pwm_dc_min:
-        pwm_dc = pwm_dc_min
-
-    # >>>>>>>>> CONVERT INTO UINT16 FORMAT
+    pwm_max = 255
+    pwm_min = 70 # ~140 rpm
+    if pwm > pwm_max:
+        pwm = pwm_max
+    elif pwm < pwm_min:
+        pwm = pwm_min
 
     # Publish to motor_input (will then be transmitted via i2c to motor)
-    pub = rospy.Publisher('motor_input', UInt16, queue_size=10)   
-    rospy.loginfo(pwm_dc)
-    pub.publish(pwm_dc)
+    pub = rospy.Publisher('motor_input', UInt16, queue_size=10)
+    rospy.loginfo(pwm)
+    pub.publish(pwm)
 
     return
 
@@ -40,23 +41,14 @@ def convert_to_volts_callback(r):
 def get_rpms_callback(r):
     global current_rpms
     current_rpms = r.data
-    print("rpms_callback %d", current_rpms)
-    
-    return   
 
-#def callback(rpms_info, control_info):
-#    print("Hi")
+    return
 
 
 def rpms2volts():
     rospy.init_node('rpms_to_volts', anonymous=True)
     rospy.Subscriber('rpms', UInt16, get_rpms_callback)
     rospy.Subscriber('control_effort', Float64, convert_to_volts_callback)
-    
-    #rpms_sub = message_filters.Subscriber('rpms', UInt16)
-    #control_sub = message_filters.Subscriber('control_effort', Float64)
-    #ts = message_filters.TimeSynchronizer([rpms_sub, control_sub], 10)
-    #ts.registerCallback(callback)     
 
     rospy.spin()
 
