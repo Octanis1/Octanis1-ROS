0e0cfcb514e3a470bab47fae361a806d59200ebb
RickCM <rick@citycable.ch>
2016-07-29 05:04:54 -0500

    Adding the launch file for creating the octomap

--- catkin_ws/src/lidar/script-run2.sh
+++ catkin_ws/src/lidar/script-run2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd $HOME/Octanis1-ROS/catkin_ws
-xterm -hold -e "rosbag play --clock src/lidar/LIDAR_IMU_TF_MAVLINK_OLIMEX_2016-07-04-21-55-39.bag"& 
- 
-xterm -hold -e "source ./devel/setup.bash ; roslaunch lidar lidar.launch"&
+cd ..
+cd .. 
+xterm -hold -e "rosbag play --clock LIDAR_IMU_TF_MAVLINK_OLIMEX_2016-07-04-21-55-39.bag"& 
 
 xterm -hold -e "source ./devel/setup.bash ; rosrun lidar point2cloud"&
 
@@ -11,6 +10,8 @@ xterm -hold -e "source ./devel/setup.bash; roslaunch lidar pointCloudAssembler.l
 
 xterm -hold -e "source ./devel/setup.bash; rosrun lidar periodic_snapshotter"&
 
-xterm -hold -e "source ./devel/setup.bash; rosrun point_cloud_converter converter points2_in:=/my_input_pointcloud2_topic points_out:=/my_output_pointcloud_topic"
+xterm -hold -e "source ./devel/setup.bash; rosrun point_cloud_converter converter points2_in:=/my_input_pointcloud2_topic points_out:=/my_output_pointcloud_topic" &
+
+xterm -hold -e "source ./devel/setup.bash; roslaunch octomap_server octomap_mapping.launch"
 
 exit 0
