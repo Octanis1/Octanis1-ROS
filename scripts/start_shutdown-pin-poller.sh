34d80a21023a56c2148f31e504281ba535d67a66
Sam Sulaimanov <sam@bitmorse.com>
2016-09-04 00:20:54 +0000

    hardcoding home...

--- scripts/start_shutdown-pin-poller.sh
+++ scripts/start_shutdown-pin-poller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -u ${HOME}/Octanis1-ROS/scripts/shutdown-pin-poller.py &>> /var/log/octanis1-ros-scripts.log &
+python -u /home/rover/Octanis1-ROS/scripts/shutdown-pin-poller.py &>> /var/log/octanis1-ros-scripts.log &
 echo $! > /var/run/shutdown-pin-poller.pid
